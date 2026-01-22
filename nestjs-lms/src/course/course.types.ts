5d8c5f8645f994c49f771c25de6eeaa1cca11694
mirajHossen99 <mirajhossen011211@gmail.com>
2026-01-22 15:28:46 +0600

    added course

---
+++ nestjs-lms/src/course/course.types.ts
@@ -0,0 +1,5 @@
+export enum CourseLevel {
+  LEVEL1 = 'level-1',
+  LEVEL2 = 'level-2',
+  LEVEL3 = 'level-3',
+}
---
+++ nestjs-lms/src/course/schemas/course.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { CourseLevel } from '../course.types';
+
+export type CourseDocument = HydratedDocument<Course>;
+
+@Schema()
+export class Course {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({
+    required: true,
+    enum: CourseLevel,
+    default: CourseLevel.LEVEL1,
+  })
+  level: CourseLevel;
+
+  @Prop({ required: true })
+  price: string;
+}
+
+export const CourseSchema = SchemaFactory.createForClass(Course);
